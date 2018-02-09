c5f4ace4fc334c68bb8a727b77333356bed9f8bd
theFOng <alecsanf@usc.edu>
2018-02-08 18:47:54 -0800

    linear regresion works for simple cases

--- Assignment-1/hw1_lr.py
+++ Assignment-1/hw1_lr.py
@@ -15,22 +15,34 @@ class LinearRegression:
         self.nb_features = nb_features
 
     def train(self, features: List[List[float]], values: List[float]):
-        """TODO : Complete this function"""
-        raise NotImplementedError
+        # creating f(x) = W0 + sum((Wd)(Xd))
+        # finding W's
+        # use LMS(least mean squared(minimizing residuals sum(mean squared error)))
+        # LMS = ((X^tX)^-1) (X^tY) (X^tX -> X^tX + lI)
+        # make sure N > D+1
+        # features ~ X values ~ y
+        assert self.nb_features == len(features)
+        y = numpy.array([values]).transpose()
+        x = numpy.array(features)
+        # to add w0
+        x = numpy.append(x, [[1]]*len(features), axis=1)
+
+        xtx = x.transpose().dot(x)
+        xty = x.transpose().dot(y)
+        xtxInv = numpy.linalg.inv(xtx)
+        self.weights = xtxInv.dot(xty)
+        
 
     def predict(self, features: List[List[float]]) -> List[float]:
-        """TODO : Complete this function"""
-        raise NotImplementedError
+        # f(x) = wx + w0
+        return numpy.add(numpy.dot(self.weights,features.transpose()), self.weights[1][0])[0]
 
     def get_weights(self) -> List[float]:
-        """TODO : Complete this function"""
-
         """
         for a model y = 1 + 3 * x_0 - 2 * x_1,
         the return value should be [1, 3, -2].
         """
-        raise NotImplementedError
-
+        return self.weights
 
 class LinearRegressionWithL2Loss:
     '''Use L2 loss for weight regularization'''
--- Assignment-1/utils.py
+++ Assignment-1/utils.py
@@ -5,8 +5,11 @@ import numpy as np
 
 def mean_squared_error(y_true: List[float], y_pred: List[float]) -> float:
     assert len(y_true) == len(y_pred)
-
-    raise NotImplementedError
+    # sum (( actual - predicted ) ^2) /n
+    totalError = 0.0
+    for i in range(0, len(y_pred)):
+        totalError += np.power((y_true[i] - y_pred[i]), 2)
+    return totalError / len(y_pred)
 
 
 def f1_score(real_labels: List[int], predicted_labels: List[int]) -> float:
@@ -38,19 +41,54 @@ def gaussian_kernel_distance(
     raise NotImplementedError
 
 
-def normalize(features: List[List[float]]) -> List[List[float]]:
-    """
-    normalize the feature vector for each sample . For example,
-    if the input features = [[3, 4], [1, -1], [0, 0]],
-    the output should be [[0.6, 0.8], [0.707107, -0.707107], [0, 0]]
-    """
-    raise NotImplementedError
+class NormalizationScaler:
+    def __init__(self):
+        pass
+
+    def __call__(self, features: List[List[float]]) -> List[List[float]]:
+        """
+        normalize the feature vector for each sample . For example,
+        if the input features = [[3, 4], [1, -1], [0, 0]],
+        the output should be [[0.6, 0.8], [0.707107, -0.707107], [0, 0]]
+        """
+        raise NotImplementedError
 
 
-def min_max_scale(features: List[List[float]]) -> List[List[float]]:
+class MinMaxScaler:
     """
-    normalize the feature vector for each sample . For example,
-    if the input features = [[2, -1], [-1, 5], [0, 0]],
-    the output should be [[1, 0], [0, 1], [0.333333, 0.16667]]
+    You should keep some states inside the object.
+    You can assume that the parameter of the first __call__
+        must be the training set.
+
+    Note:
+        1. you may assume the parameters are valid when __call__
+            is being called the first time (you can find min and max).
+
+    Example:
+        train_features = [[0, 10], [2, 0]]
+        test_features = [[20, 1]]
+
+        scaler = MinMaxScale()
+        train_features_scaled = scaler(train_features)
+        # now train_features_scaled should be [[0, 1], [1, 0]]
+
+        test_features_sacled = scaler(test_features)
+        # now test_features_scaled should be [[10, 0.1]]
+
+        new_scaler = MinMaxScale() # creating a new scaler
+        _ = new_scaler([[1, 1], [0, 0]]) # new trainfeatures
+        test_features_scaled = new_scaler(test_features)
+        # now test_features_scaled should be [[20, 1]]
+
     """
-    raise NotImplementedError
+
+    def __init__(self):
+        pass
+
+    def __call__(self, features: List[List[float]]) -> List[List[float]]:
+        """
+        normalize the feature vector for each sample . For example,
+        if the input features = [[2, -1], [-1, 5], [0, 0]],
+        the output should be [[1, 0], [0, 1], [0.333333, 0.16667]]
+        """
+        raise NotImplementedError
