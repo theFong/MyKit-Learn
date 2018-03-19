353ed0f045c6349b62c66092da7929fe58feb243
567bot <brandon.holden@usc.edu>
2018-03-19 16:21:39 -0700

    [course staff] Assignment-4

---
+++ Assignment-4/gmm.py
@@ -0,0 +1,116 @@
+import numpy as np
+from kmeans import KMeans
+
+
+class GMM():
+    '''
+        Fits a Gausian Mixture model to the data.
+
+        attrs:
+            n_cluster : Number of mixtures
+            e : error tolerance
+            max_iter : maximum number of updates
+            init : initialization of means and variance
+                Can be 'random' or 'kmeans'
+            means : means of gaussian mixtures
+            variances : variance of gaussian mixtures
+            pi_k : mixture probabilities of different component
+    '''
+
+    def __init__(self, n_cluster, init='k_means', max_iter=100, e=0.0001):
+        self.n_cluster = n_cluster
+        self.e = e
+        self.max_iter = max_iter
+        self.init = init
+        self.means = None
+        self.variances = None
+        self.pi_k = None
+
+    def fit(self, x):
+        '''
+            Fits a GMM to x.
+
+            x: is a NXD size numpy array
+            updates:
+                self.means
+                self.variances
+                self.pi_k
+        '''
+        assert len(x.shape) == 2, 'x can only be 2 dimensional'
+
+        np.random.seed(42)
+        N, D = x.shape
+
+        if (self.init == 'k_means'):
+            # TODO
+            # - comment/remove the exception
+            # - initialize means using k-means clustering
+            # - compute variance and pi_k
+
+            # DONOT MODIFY CODE ABOVE THIS LINE
+            raise Exception(
+                'Implement initialization of variances, means, pi_k using k-means')
+            # DONOT MODIFY CODE BELOW THIS LINE
+
+        elif (self.init == 'random'):
+            # TODO
+            # - comment/remove the exception
+            # - initialize means randomly
+            # - compute variance and pi_k
+
+            # DONOT MODIFY CODE ABOVE THIS LINE
+            raise Exception(
+                'Implement initialization of variances, means, pi_k randomly')
+            # DONOT MODIFY CODE BELOW THIS LINE
+
+        else:
+            raise Exception('Invalid initialization provided')
+
+        # TODO
+        # - comment/remove the exception
+        # - find the optimal means, variances, and pi_k and assign it to self
+        # - return number of updates done to reach the optimal values.
+        # Hint: Try to seperate E & M step for clarity
+
+        # DONOT MODIFY CODE ABOVE THIS LINE
+        raise Exception('Implement fit function (filename: gmm.py)')
+        # DONOT MODIFY CODE BELOW THIS LINE
+
+    def sample(self, N):
+        '''
+        sample from the GMM model
+
+        N is a positive integer
+        return : NXD array of samples
+
+        '''
+        assert type(N) == int and N > 0, 'N should be a positive integer'
+        np.random.seed(42)
+        if (self.means is None):
+            raise Exception('Train GMM before sampling')
+
+        # TODO
+        # - comment/remove the exception
+        # - generate samples from the GMM
+        # - return the samples
+
+        # DONOT MODIFY CODE ABOVE THIS LINE
+        raise Exception('Implement sample function in gmm.py')
+        # DONOT MODIFY CODE BELOW THIS LINE
+
+    def compute_log_likelihood(self, x):
+        '''
+            Return log-likelihood for the data
+
+            x is a NXD matrix
+            return : a float number which is the log-likelihood of data
+        '''
+        assert len(x.shape) == 2,  'x can only be 2 dimensional'
+        # TODO
+        # - comment/remove the exception
+        # - calculate log-likelihood using means, variances and pi_k attr in self
+        # - return the log-likelihood
+        # Note: you can call this function in fit function (if required)
+        # DONOT MODIFY CODE ABOVE THIS LINE
+        raise Exception('Implement compute_log_likelihood function in gmm.py')
+        # DONOT MODIFY CODE BELOW THIS LINE
